0cb79533f6d7c2dff55cd05702f0972a51454d2a
LexisM <55384632+LexisM@users.noreply.github.com>
2021-02-12 13:15:04 +0000

    Home page in progress

---
+++ JAVA/js_index.js
@@ -0,0 +1,23 @@
+function move(){
+	if (this.scrollY > this.innerHeight / 5){
+		document.querySelector(".header_image").classList.remove("clip-up");
+		  var element, name, arr;
+		  element = document.querySelector(".header_image");
+		  name = "clip-down";
+		  arr = element.className.split(" ");
+		  if (arr.indexOf(name) == -1) {
+		    element.className += " " + name;
+			}
+			
+		}else{
+			document.querySelector(".header_image").classList.remove("clip-down");
+			var element, name, arr;
+		  element = document.querySelector(".header_image");
+		  name = "clip-up";
+		  arr = element.className.split(" ");
+		  if (arr.indexOf(name) == -1) {
+		    element.className += " " + name;
+			}
+		}
+}
+ window.addEventListener("scroll", move);
